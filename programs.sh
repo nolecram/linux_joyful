16d5e449186e4471785e3240c5d07454fd408bd2
Marco Celon <20218924+nolecram@users.noreply.github.com>
2023-03-12 01:16:38 +1100

    Create

---
+++ programs.sh
@@ -0,0 +1,30 @@
+# Show a Steam locomotive
+sl
+# Show a burning fire
+aafire
+# Continuosly repeat your string
+yes
+# Tells you a "said"
+fortune
+# Rainbownize your text
+lolcat
+# Puffetize your text
+figlet
+# A variation of figlet
+toilet
+# Bannerize your text
+banner
+# Summon a mouse following cat
+oneko
+# Show a cow saying something
+cowsay
+# A beautiful aquarium
+asciiquarium
+# The famous effect from the "Matrix" movie
+Cmatrix
+# A pair of eyes following your mouse
+xeyes
+# Let your Linux desktop speak
+espeak
+# Create a fake identity
+rig
