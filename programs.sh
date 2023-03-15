88a47ccd3a6471164cc0943116187aa5242e1703
Marco Celon <20218924+nolecram@users.noreply.github.com>
2023-03-15 13:41:34 +1100

    Update programs.sh

--- programs.sh
+++ programs.sh
@@ -1,34 +1,50 @@
 # Show a Steam locomotive
 sl
+
 # Show a burning fire
 aafire
+
 # Continuosly repeat your string
 yes
+
 # Tells you a "said"
 fortune
+
 # Rainbownize your text
 lolcat
+
 # Puffetize your text
 figlet
+
 # A variation of figlet
 toilet
+
 # Bannerize your text
 banner
+
 # Summon a mouse following cat
 oneko
+
 # Show a cow saying something
 cowsay
+
 # A beautiful aquarium
 asciiquarium
-# The famous effect from the "Matrix" movie
+
+#The famous effect from the "Matrix" movie
 Cmatrix
+
 # A pair of eyes following your mouse
 xeyes
+
 # Let your Linux desktop speak
 espeak
+
 # Create a fake identity
 rig
+
 # It reverses every string given to it
 rev
+
 # Output all the possible factors of a given number
 factor
